4650dad19269b4985880833074edb75af4ef846f
josh <jstaboon@gmail.com>
2017-08-29 23:39:36 -0700

    1
    1

---
+++ auto.py
@@ -0,0 +1,21 @@
+import threading
+import os, subprocess
+
+def process():
+    print "working"
+    cmds = 'cd C:\Users\WORK\Documents\GitHub\gitgis'
+    subprocess.call(cmds, shell=True)
+    cmds = 'python sample2.py'
+    subprocess.call(cmds, shell=True)
+    cmds = 'git commit'
+    subprocess.call(cmds, shell=True)
+    cmds = 'git push'
+    subprocess.call(cmds, shell=True)
+    print "waiting"
+    threading.Timer(60, process).start()
+    
+
+process()
+
+
+
